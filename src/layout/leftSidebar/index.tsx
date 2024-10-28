070aa5115636417e07179ecb3fb4e2c0c1393b7a
Jaison John <jaisonjohny26@gmail.com>
2024-10-28 16:52:08 +0000

    sidebar style changes

--- src/layout/leftSidebar/index.tsx
+++ src/layout/leftSidebar/index.tsx
@@ -6,15 +6,30 @@ const LeftSidebar = () => {
       className="sticky  top-16 h-48
         w-48"
     >
-      <ul className="list-none pt-4  font-semibold text-busanJames flex flex-col gap-4">
-        <li>
-          <Link to="#">Create Exam</Link>
+      <ul className="list-none pt-4  font-semibold text-busanJames flex flex-col gap-4 w-full">
+        <li className="w-full">
+          <Link
+            to="#"
+            className="hover:bg-busanJames hover:text-white p-1 block rounded w-full"
+          >
+            Create Exam
+          </Link>
         </li>
         <li>
-          <Link to="#">Exams</Link>
+          <Link
+            to="#"
+            className="hover:bg-busanJames hover:text-white p-1 block rounded w-full"
+          >
+            Exams
+          </Link>
         </li>
         <li>
-          <Link to="#">Members</Link>
+          <Link
+            to="#"
+            className="hover:bg-busanJames hover:text-white p-1 block rounded w-full"
+          >
+            Members
+          </Link>
         </li>
       </ul>
     </div>
